62ac6f2e0f552fc2e1654f03bc37036f9e3ba2e0
pullani <mah.pullani@gmail.com>
2020-06-11 19:55:26 +0530

    Updates

---
+++ precache-manifest.1758f5aca11e277add95f5ae9b49c39b.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "66159551066dd51cd4799983b69bd7bb",
+    "url": "/react-app-redux/index.html"
+  },
+  {
+    "revision": "55ecc93f52462b2b1510",
+    "url": "/react-app-redux/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "12388c93aed039f5dc42",
+    "url": "/react-app-redux/static/css/main.1549ba50.chunk.css"
+  },
+  {
+    "revision": "55ecc93f52462b2b1510",
+    "url": "/react-app-redux/static/js/2.0fb2d2f3.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/react-app-redux/static/js/2.0fb2d2f3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "12388c93aed039f5dc42",
+    "url": "/react-app-redux/static/js/main.413b8e00.chunk.js"
+  },
+  {
+    "revision": "051bb8d34727cb9bc096",
+    "url": "/react-app-redux/static/js/runtime-main.117b2628.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/react-app-redux/static/media/SEGA.12a3db8b.woff"
+  }
+]);
